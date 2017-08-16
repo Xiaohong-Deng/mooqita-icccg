81f6afe3ca34ed6cf80236e894354bc4dadf68f1
nicenick <worldsky86rough@gmail.com>
2017-08-16 12:05:29 +0800

    Add home page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = "ICCCG"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def current_user
+    nil
+  end
 end
