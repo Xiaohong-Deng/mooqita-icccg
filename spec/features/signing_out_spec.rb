196de4213f48a575bcfae011d54709bd085b710f
nicenick <worldsky86rough@gmail.com>
2017-08-19 15:51:30 +0800

    Add tests for authentication

---
+++ spec/features/signing_out_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.feature 'Signed-in users can sign out' do
+  let!(:user) { FactoryGirl.create(:user) }
+
+  before do
+    login_as(user)
+  end
+
+  scenario do
+    visit root_path
+    click_link "Log out"
+    expect(page).to have_content("Signed out successfully.")
+  end
+end
