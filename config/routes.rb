ee78d4062cb2741c115a0be661b0d32b517abf4a
nicenick <worldsky86rough@gmail.com>
2017-08-19 12:22:09 +0800

    Install and configure devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   devise_for :users
-=======
->>>>>>> Add home page
+
   root 'static_pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
