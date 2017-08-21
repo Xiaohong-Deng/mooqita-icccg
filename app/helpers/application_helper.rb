4e7034740941294b9c32a52046bb75094209dc46
nicenick <worldsky86rough@gmail.com>
2017-08-21 13:07:41 +0800

    Make auth flash msg style right

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,13 @@ module ApplicationHelper
   def current_user
     nil
   end
+
+  def flash_class(level)
+    case level
+    when 'notice' then "alert alert-success"
+    when 'success' then "alert alert-success"
+    when 'error' then "alert alert-warning"
+    when 'alert' then "alert alert-danger"
+    end
+  end
 end
