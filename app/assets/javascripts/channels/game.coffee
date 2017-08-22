8fab3eb16b79f88437a50116ec3a4a49964ed980
Jacob Wald <email@example.com>
2017-08-21 21:55:52 -0400

    add action cable

---
+++ app/assets/javascripts/channels/game.coffee
@@ -0,0 +1,3 @@
+# App.room = App.cable.subscriptions.create channel: "GameChannel",
+#   received: (data) ->
+#     $('.status').html data['status']
--- app/models/game_user.rb
+++ app/models/game_user.rb
@@ -2,9 +2,24 @@ class GameUser < ApplicationRecord
   belongs_to :game
   belongs_to :user
 
-  after_save :update_game_status
-  
-  def update_game_status
-    game.active! if game.number_of_players == 3
+  after_create_commit do
+    update_game_status!
+
+    if game.active?
+      GameStartingBroadcastJob.perform_now(game)
+    else
+      StatusBroadcastJob.perform_now(game)
+    end
+  end
+
+  after_destroy_commit { StatusBroadcastJob.perform_now(game) }
+
+  private
+
+  def update_game_status!
+    if game.number_of_players == 3
+      game.active!
+
+    end
   end
 end
