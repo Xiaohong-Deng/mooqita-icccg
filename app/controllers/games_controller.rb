029f80917596d57f1e7ffe701467d0aab96d6528
Jacob Wald <email@example.com>
2017-08-25 15:39:43 -0400

    add new route for game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,9 @@
 class GamesController < ApplicationController
   def waiting_room
   end
+
+  def show
+    #restrict only logged in user and player of the current game
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   root 'static_pages#home'
-
   get '/games/waiting-room', to: 'games#waiting_room'
+  resources :games, only: [:show]
 
   mount ActionCable.server => '/cable'
 end
