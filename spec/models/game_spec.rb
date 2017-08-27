280eda15c176521c60cd92e6f550aaedde8760af
Jacob Wald <email@example.com>
2017-08-27 15:01:07 -0400

    add game spec

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.create_with_users_ids' do
+    it 'creates a new game' do
+      Game.create_with_users_ids([])
+
+      expect(Game.all.size).to eq 1
+    end
+
+    context 'ids given' do
+      it 'adds each user that matches id to the game' do
+        user1 = User.create(email: "user1@example.com", password: "topsecret")
+        user2 = User.create(email: "user2@example.com", password: "topsecret")
+
+        game = Game.create_with_users_ids([1, 2])
+        expect(game.players.to_a).to include(user1, user2)
+      end
+    end
+  end
 end
