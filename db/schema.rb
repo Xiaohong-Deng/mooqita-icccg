0a546ce072a7f5bea77acb785ad673b61f54f6f8
nicenick <worldsky86rough@gmail.com>
2017-09-03 01:00:32 +0800

    Add model document and random_fetch method to it with rspec test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170825183045) do
+ActiveRecord::Schema.define(version: 20170902161416) do
+
+  create_table "documents", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "game_players", force: :cascade do |t|
     t.integer "game_id"
