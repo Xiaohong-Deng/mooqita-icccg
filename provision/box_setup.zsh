ee5b75dd21ac020956ef1b23f2fd09f8e63ac610
David O' Rojo <david.orojo@gmail.com>
2017-09-03 22:29:33 -0500

    Autoaccept Yarn package installation to avoid breaking the provision script

--- provision/box_setup.zsh
+++ provision/box_setup.zsh
@@ -109,5 +109,5 @@ echo "***************************************************"
 if ! dpkg -s yarn; then
  curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
  echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
- sudo apt-get update && sudo apt-get install yarn
+ sudo apt-get update && sudo apt-get -y install yarn
 fi
