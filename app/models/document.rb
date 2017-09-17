7daf94cb60f2867bd52ac81328820a8b5b5a23f9
nicenick <worldsky86rough@gmail.com>
2017-09-17 15:38:53 +0800

    update model files reflecting one to many relationship between games and documents

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,4 +1,6 @@
 class Document < ApplicationRecord
+  has_many :games, dependent: :destroy
+
   def self.random_fetch
     Document.all.order("RANDOM()").first
   end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,13 +1,13 @@
 class Game < ApplicationRecord
   has_many :game_players, dependent: :destroy
   has_many :players, through: :game_players, source: :user
+  belongs_to :document
 
   enum status: [:active, :paused]
 
   def self.create_with_users_ids(ids)
-    game = Game.create
+    game = Game.create(document: Document.random_fetch)
     roles_shuffled = GAME_ROLES.map(&:to_s).shuffle
-
 
     ids.zip(roles_shuffled).each do |id, role|
       game.game_players.create(user_id: id, role: role)
