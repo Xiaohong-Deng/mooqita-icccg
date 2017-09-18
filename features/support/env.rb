d190f224a9872bddf45d17b89f5fddf5db356c3c
nicenick <worldsky86rough@gmail.com>
2017-09-18 13:42:05 +0800

    add support for js test in cucumber

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,7 +5,17 @@
 # files.
 
 require 'cucumber/rails'
+require "capybara/cucumber"
 
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
+end
+
+Capybara.javascript_driver = :chrome
+
+Capybara.configure do |config|
+  config.default_max_wait_time = 10
+end
 # Capybara defaults to CSS3 selectors rather than XPath.
 # If you'd prefer to use XPath, just uncomment this line and adjust any
 # selectors in your step definitions to use the XPath syntax.
