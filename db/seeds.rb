c733edf16d63dbbcbaca548ba1b016668ee6d10b
David O' Rojo <david.orojo@gmail.com>
2017-09-19 11:09:44 -0500

    Make the title field contain only that information for a document

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,12 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 (1..4).each do |n|
-  User.create email: "user#{n}@example.com", password: "topsecret"
+ User.create email: "user#{n}@example.com", password: "topsecret"
 end
 
 Dir.glob('db/documents/*.txt').each do |file_name|
   lines = File.open(file_name, 'r').readlines
-  title = lines[0].chomp,
+  title = lines[0].chomp
   content = lines.slice(2, lines.length - 2).join.chomp
 
   Document.create title: title, content: content
