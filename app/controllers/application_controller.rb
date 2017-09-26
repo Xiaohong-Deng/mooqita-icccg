1d7766bd50a0617aef349add1d8960f06cb28d94
nicenick <worldsky86rough@gmail.com>
2017-09-26 20:23:32 +0800

    reorganize authorization methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  private
+    def authenticate_player!
+      unless @game.players.include? current_user
+        flash[:danger] = 'You are not part of the game'
+        redirect_to root_path
+      end
+    end
 end
