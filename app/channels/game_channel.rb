b346daca083964bca6c2d900ed86f65fff8140f1
nicenick <worldsky86rough@gmail.com>
2017-09-27 12:39:24 +0800

    refactor GameChannel#receive

--- app/channels/game_channel.rb
+++ app/channels/game_channel.rb
@@ -15,20 +15,26 @@ class GameChannel < ApplicationCable::Channel
     # receive from actioncable js client side code
     # could be question or answer
     # it depends on js code to send a single key hash
-    model_name = payload.first[0].to_s.classify.constantize
-    round = @game_player.round
-    attributes = {game_id: params['game'], round: round, content: payload.first[1]}
-    if question = @game.questions.find_by(round: round)
-      attributes[:question_id] = question.id
-    end
-    attributes.select! { |x| model_name.attribute_names.index(x.to_s) }
-    attributes[:user] = current_user
-    model_name.create(attributes)
+    payload_pair = payload.first
+    model_name = payload_pair[0].to_s.classify.constantize
+    content = payload_pair[1]
+    model_name.create model_params(model_name, content)
   end
 
   private
     def set_game_and_player
       @game = Game.find(params['game'])
       @game_player = @game.game_players.find_by(user: current_user)
+    end
+
+    def model_params(model_name, content)
+      round = @game_player.round
+      params = {game_id: params['game'], round: round, content: content}
+      if question = @game.questions.find_by(round: round)
+        params[:question_id] = question.id
+      end
+      params.select! { |x| model_name.attribute_names.index(x.to_s) }
+      params[:user] = current_user
+      params
     end
 end
