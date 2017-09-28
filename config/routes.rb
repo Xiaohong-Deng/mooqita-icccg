c4bbb2b6c1ba4d15a63227d9bc8f71e6bb219fa9
nicenick <worldsky86rough@gmail.com>
2017-09-28 15:47:24 +0800

    add route for judge form

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,11 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
   get '/games/waiting-room', to: 'game_waiting_rooms#index'
-  resources :games, only: [:show, :update]
+  resources :games, only: [:show, :update] do
+    member do
+      get :judge # send a judge form to judge to let him choose the right answer
+    end
+  end
   resources :questions, only: [:create]
   resources :answers, only: [:create]
 
