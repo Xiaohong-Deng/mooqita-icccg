3f4766eb4c8a4c449871297c488c721179c08fc4
nicenick <worldsky86rough@gmail.com>
2017-09-28 15:37:54 +0800

    make one question two answers per game per round in models

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,7 @@ class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
   belongs_to :game
   belongs_to :user
+  validates :game_id, uniqueness: { scope: :round }
 
   after_create_commit { QuestionBroadcastJob.perform_now self }
 end
