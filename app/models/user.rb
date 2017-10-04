dab090d0884b65932e8d9f075a1733e8d606424d
nicenick <worldsky86rough@gmail.com>
2017-10-05 06:22:17 +0800

    update players scores

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :game_players, dependent: :destroy
+  has_many :games, through: :game_players
 end
