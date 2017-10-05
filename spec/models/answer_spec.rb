59df7c25880a6608f5b4d2b179ba5f526d1ca1ab
nicenick <worldsky86rough@gmail.com>
2017-10-06 01:09:40 +0800

    fix rspec tests

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,14 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  let(:user) { FactoryGirl.create(:user) }
+  let(:user1) { FactoryGirl.create(:user) }
+  let(:user2) { FactoryGirl.create(:user) }
   let(:document) { FactoryGirl.create(:document) }
   let(:game) { FactoryGirl.create(:game, document: document) }
-  let(:question) { FactoryGirl.create(:question, game: game, user: user) }
+  let(:question) { FactoryGirl.create(:question, game: game, user: user1) }
 
   before do
-    @answer1 = Answer.create(content: "I ate yesterday", user: user, question: question)
-    @answer2 = Answer.create(content: "I did't eat yesterday", user: user, question: question, judge_choice: true)
+    @answer1 = Answer.create(content: "I ate yesterday", user: user1, question: question)
+    @answer2 = Answer.create(content: "I did't eat yesterday", user: user2, question: question, judge_choice: true)
   end
 
   it 'can set itself to judge identified' do
