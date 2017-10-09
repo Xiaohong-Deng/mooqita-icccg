a77fe196c4f3f4d6579d719078c5d4225d9a86f0
nicenick <worldsky86rough@gmail.com>
2017-10-09 09:14:31 +0800

    show past question answer pairs

--- spec/features/shared_examples/whiteboard.rb
+++ spec/features/shared_examples/whiteboard.rb
@@ -3,5 +3,10 @@ RSpec.shared_examples "whiteboard" do
     within(".titles") do
       expect(page).to have_content "Whiteboard"
     end
+
+    within("#whiteboard") do
+      expect(page).to have_content "Did you eat anything yesterday?"
+      expect(page).to have_content "I ate yesterday"
+    end
   end
 end
